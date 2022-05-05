34ef602df21add5fdfd96ad4102e2700664f6635
Manuel López Anido <mlopez@liliana.com.ar>
2022-05-05 16:56:43 -0300

    new version of abastecimiento

--- controllers/abastecimiento.js
+++ controllers/abastecimiento.js
@@ -1,4 +1,4 @@
-const abasTable = require('../data samples/data2.json')
+const abasTable = require('../data samples/abastecimientoTable.json')
 const abasUser = require('../data samples/abastecimientoUsers.json')
 const abasInputs = require ('../data samples/abastecimientoInputs.json');
 
@@ -15,8 +15,6 @@ const login = (req,res)=>{
     lider.user===loginData.lider
     )
   })
-  console.log('index: ',index)
-  console.log('password: ',abasUser[index].password)
   if (abasUser[index].password === "") {
     abasUser[index].password = loginData.contraseña
     fs.writeFile('./data samples/abastecimientoUsers.json',JSON.stringify(abasUser,null,2),function (err){
@@ -34,32 +32,86 @@ const login = (req,res)=>{
 const uploadInput = (req,res)=>{
   const abas = req.body
   abasInputs.push(abas)
-  console.log(abas)
+  console.log('Datos ingresado', abas)
   fs.writeFile('./data samples/abastecimientoInputs.json',JSON.stringify(abasInputs,null,2),function (err){
     if (err) throw (err);
   })
-  console.log(abasTable[3].estanteria)
-  console.log(abas.estanteria)
   let posIndex = abasTable.findIndex((pos)=>{
     return(pos.estanteria===abas.estanteria && pos.posicion===abas.posicion && pos.altura===abas.altura)
   })
   console.log('index: ', posIndex)
-  console.log(abasTable[posIndex])
+  console.log('Datos encontrados',abasTable[posIndex])
+  
   if (!abasTable[posIndex]){
     res.status(401).send({message:'La estanteria no existe!'})
   }else{
-    if (abasTable[posIndex].codigo===abas.codigo){
-      abasTable[posIndex].cantidad=abasTable[posIndex].cantidad + abas.cantidad
-      abasTable[posIndex].time = abas.time
-      abasTable[posIndex].date = abas.date
-      if  (abasTable[posIndex].cantidad < 0){
-        abasTable[posIndex].cantidad=0
-      }
-    }else{
-      abasTable[posIndex].codigo = abas.codigo
-      abasTable[posIndex].cantidad = abas.cantidad
-      abasTable[posIndex].time = abas.time
-      abasTable[posIndex].date = abas.date
+    abasTable[posIndex].time = abas.time
+    abasTable[posIndex].date = abas.date
+    abasTable[posIndex].radio = abas.radio
+    switch (abas.radio){
+      case 'add':
+        //First looks if insmuos is empty
+        if(!abasTable[posIndex].insumos[0].codigo){
+          abasTable[posIndex].insumos=[]
+        }
+        //Second, look if it is already in the position
+        const indexAdd = abasTable[posIndex].insumos.findIndex((insumo)=>{
+          return(
+            insumo.codigo === abas.codigo
+          )
+        })
+        if (indexAdd === -1){
+          abasTable[posIndex].insumos.push({
+            ['codigo']:abas.codigo,
+            ['cantidad']:abas.cantidad
+          })
+        } else {
+          abasTable[posIndex].insumos[indexAdd].cantidad += abas.cantidad
+        }
+        break
+      case 'replace':
+        abasTable[posIndex].insumos=[{
+          ['codigo']:abas.codigo,
+          ['cantidad']:abas.cantidad
+        }]
+        break
+      case 'down':
+        //First, look if it is already in the position
+        const indexDown = abasTable[posIndex].insumos.findIndex((insumo)=>{
+          return(
+            insumo.codigo === abas.codigo
+          )
+        })
+        if (indexDown === -1){
+          res.status(401).send({message:'Este insumo no se encuentra en la estanteria!'})
+        } else {
+          if (!abas.cantidad){
+            const insumoToRemove = abasTable[posIndex].insumos[indexDown]
+            abasTable[posIndex].insumos.splice(indexDown,1)
+            res.status(200).send(insumoToRemove)
+          } else {
+            abasTable[posIndex].insumos[indexDown].cantidad -= abas.cantidad
+            if (abasTable[posIndex].insumos[indexDown].cantidad <= 0) {
+              abasTable[posIndex].insumos.splice(indexDown,1)
+            }
+          }
+          // if insumos got empty, push one empty object element
+          if (!abasTable[posIndex].insumos.length){
+            abasTable[posIndex].insumos.push({
+              codigo:'',
+              cantidad: 0
+            })
+          }
+        }
+        break
+      case 'clean':
+        abasTable[posIndex].insumos=[{
+          codigo:'',
+          cantidad: 0
+        }]
+        break
+      default:
+        res.status(401).send({message:'Ingreso no válido'})
     }
   }
   fs.writeFile('./data samples/abastecimientoTable.json',JSON.stringify(abasTable,null,2),function (err){
