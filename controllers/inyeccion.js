9eb0140aa3b7b223862677bb8f789fdfee6514b4
Manuel López Anido <mlopez@liliana.com.ar>
2022-08-17 11:35:13 -0300

    armado modification

--- controllers/inyeccion.js
+++ controllers/inyeccion.js
@@ -1,14 +1,21 @@
 const db = require ('../config.js')
 
 const dataProd = require ('../../'+db+'/productionTable.json')
-const inyeccionUser = require('../../'+db+'/inyeccionUsers.json')
+
 
 const fs = require('fs');
 
 const getTable = (req,res)=>{
   res.send(dataProd)
 }
+const getUsers = (req,res)=>{
+  let usersRaw = fs.readFileSync('C:/Users/mlopez/Desktop/'+db+'/inyeccionUsers.json','utf8')
+  let inyUsers = JSON.parse(usersRaw)
+  res.send(inyUsers)
+}
 const login = (req,res)=>{
+  let usersRaw = fs.readFileSync('C:/Users/mlopez/Desktop/'+db+'/inyeccionUsers.json','utf8')
+  let inyeccionUser = JSON.parse(usersRaw)
   const loginData = req.body
   console.log('login data',loginData)
   const index = inyeccionUser.findIndex(lider => {
@@ -32,4 +39,39 @@ const login = (req,res)=>{
     res.status(401).send({message:'Contraseña incorrecta'})
   }
 }
-module.exports = { getTable, login }
+const uploadInput = (req,res)=>{
+  let inyeInputsRaw = fs.readFileSync('C:/Users/mlopez/Desktop/'+db+'/inyeccionInputs.json','utf8')
+  let inyeInputs = JSON.parse(inyeInputsRaw)
+
+  const inyInput = req.body
+  
+  console.log('Datos ingresado', inyInput)
+  let posIndex = inyeInputs.findIndex((inp)=>{
+    return(
+      inp.day===inyInput.day &&
+      inp.shift === inyInput.shift  &&
+      inp.machine===inyInput.machine 
+    )
+  })
+  if (posIndex === -1){
+    inyeInputs.push(inyInput)
+  } else if (inyInput.edit) {
+    inyeInputs.splice(posIndex,1)
+    inyeInputs.push(inyInput)
+  } else {
+    res.status(401).send(
+      {
+        'message': 'Carga para esta maquina ya realizada',
+        'exist': true,
+        'inputs': inyInput
+      }
+    )
+    return
+  }
+  fs.writeFile('../'+db+'/inyeccionInputs.json',JSON.stringify(inyeInputs,null,2),function (err){
+    if (err) throw (err);
+  })
+  res.send(inyInput)
+}
+
+module.exports = {getTable, login, getUsers, uploadInput}
--- router/inyeccion.js
+++ router/inyeccion.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const inyeccion = require('../controllers/inyeccion.js');
 
 router.put('/login',inyeccion.login)
-//router.put('/upload',inyeccion.uploadInput)
+router.put('/upload',inyeccion.uploadInput)
 router.get('/tablas',inyeccion.getTable)
+router.get('/users',inyeccion.getUsers)
 
 module.exports = router
